98c1e48ebee7e4621449bb9577cdf724157913fb
toxic123MMM <aashish02@gmail.com>
2024-12-20 22:42:11 +0400

    Add files via upload

---
+++ swap numbers.py
@@ -0,0 +1,5 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+a, b, c = b, c, a
+print(f"After swapping: a = {a}, b = {b}, c = {c}")
